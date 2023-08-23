a83b395b2f7fdaf4a19bd1b39c68ccfb875995b5
Andrew <andrey@b13desgin.ru>
2023-08-24 02:32:31 +0300

    add txt file + semicolons to lines in colorDoneVar.txt output

--- app.js
+++ app.js
@@ -2,10 +2,8 @@ const fs = require('fs');
 const ColorDiff = require('color-diff');
 const colorAtlas = require('color-name-list');
 
-// Создаем палитру из атласа цветов
 const palette = colorAtlas.map(entry => entry.hex);
 
-// Преобразуем HEX цвет в RGB компоненты
 function hexToRgb(hex) {
     const r = parseInt(hex.slice(1, 3), 16);
     const g = parseInt(hex.slice(3, 5), 16);
@@ -13,48 +11,33 @@ function hexToRgb(hex) {
     return { R: r, G: g, B: b };
 }
 
-// Преобразуем RGB компоненты в HEX
 function rgbToHex(rgb) {
     return `#${rgb.R.toString(16).padStart(2, '0')}${rgb.G.toString(16).padStart(2, '0')}${rgb.B.toString(16).padStart(2, '0')}`;
 }
 
-// Преобразуем строку в верблюжий стиль
 function toCamelCase(str) {
-    return str.replace(/(?:^\w|[A-Z]|\b\w)/g, (letter, index) => {
-        return index === 0 ? letter.toLowerCase() : letter.toUpperCase();
-    }).replace(/\s+/g, '');
+    return str.replace(/(?:^\w|[A-Z]|\b\w)/g, (letter, index) => index === 0 ? letter.toLowerCase() : letter.toUpperCase()).replace(/\s+/g, '');
 }
 
-// Функция для поиска ближайшего цвета
 function findNearestColor(color) {
     const rgbColor = hexToRgb(color);
     const nearestColor = ColorDiff.closest(rgbColor, palette.map(hexToRgb));
     const nearestHexColor = rgbToHex(nearestColor);
-
     const foundColor = colorAtlas.find(entry => entry.hex.toUpperCase() === nearestHexColor.toUpperCase());
 
-    if (foundColor) {
-        return `val ${toCamelCase(foundColor.name)} = "${foundColor.hex}"`;
-    } else {
-        return "// Цвет не найден в атласе.";
-    }
+    return foundColor ? `val ${toCamelCase(foundColor.name)} = "${foundColor.hex}"` : "// Цвет не найден в атласе.";
 }
 
-// Чтение цветов из файла
 fs.readFile('color.txt', 'utf8', (err, data) => {
     if (err) {
         console.error('Ошибка при чтении файла:', err);
         return;
     }
 
-    const inputColors = data
-        .split(',')
-        .map(color => color.trim().replace(/^#/, '').toUpperCase());
-
-    const results = inputColors.map(color => findNearestColor(`#${color}`));
-
-    // Запись результатов в файл
+    const inputColors = data.split(',').map(color => color.trim().replace(/^#/, '').toUpperCase());
+    const results = inputColors.map(color => findNearestColor(`#${color}`)).map(line => line + ";");
     const output = results.join('\n');
+
     fs.writeFile('colorDoneVar.txt', output, 'utf8', err => {
         if (err) {
             console.error('Ошибка при записи файла:', err);
