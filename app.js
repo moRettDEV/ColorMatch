1b3308cf1b8d7bac4d8e04aaf922415648d13948
Andrew <andrey@b13design.ru>
2023-08-24 14:52:42 +0300

    Updated output format in colorDoneVar.txt file

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const colorAtlas = require('color-name-list');
 // Создаем массив палитры цветов из атласа
 const palette = colorAtlas.map(entry => entry.hex);
 
-// Преобразуем HEX цвет в объект RGB компонент
+// Преобразуем HEX-цвет в объект RGB компонент
 function hexToRgb(hex) {
     const r = parseInt(hex.slice(1, 3), 16);
     const g = parseInt(hex.slice(3, 5), 16);
@@ -13,27 +13,30 @@ function hexToRgb(hex) {
     return { R: r, G: g, B: b };
 }
 
-// Преобразуем объект RGB компонент в HEX цвет
+// Преобразуем объект RGB компонент в HEX-цвет
 function rgbToHex(rgb) {
-    return `#${rgb.R.toString(16).padStart(2, '0')}${rgb.G.toString(16).padStart(2, '0')}${rgb.B.toString(16).padStart(2, '0')}`;
+    const componentToHex = c => c.toString(16).padStart(2, '0');
+    return `#${componentToHex(rgb.R)}${componentToHex(rgb.G)}${componentToHex(rgb.B)}`;
 }
 
-// Преобразуем строку в верблюжий стиль
+// Преобразуем строку в стиль верблюжьей нотации
 function toCamelCase(str) {
-    return str.replace(/(?:^\w|[A-Z]|\b\w)/g, (letter, index) => index === 0 ? letter.toLowerCase() : letter.toUpperCase()).replace(/\s+/g, '');
+    return str.replace(/(?:^\w|[A-Z]|\b\w)/g, (letter, index) =>
+        index === 0 ? letter.toLowerCase() : letter.toUpperCase()
+    ).replace(/\s+/g, '');
 }
 
-// Функция для поиска ближайшего цвета
+// Находим ближайший цвет из палитры
 function findNearestColor(color) {
     const rgbColor = hexToRgb(color);
     const nearestColor = ColorDiff.closest(rgbColor, palette.map(hexToRgb));
     const nearestHexColor = rgbToHex(nearestColor);
     const foundColor = colorAtlas.find(entry => entry.hex.toUpperCase() === nearestHexColor.toUpperCase());
 
-    return foundColor ? `val ${toCamelCase(foundColor.name)} = "${foundColor.hex}"` : "// Цвет не найден в атласе.";
+    return foundColor ? `<color name="${toCamelCase(foundColor.name)}">${foundColor.hex}</color>` : "";
 }
 
-// Чтение цветов из файла
+// Читаем цвета из файла
 fs.readFile('color.txt', 'utf8', (err, data) => {
     if (err) {
         console.error('Ошибка при чтении файла:', err);
@@ -43,13 +46,11 @@ fs.readFile('color.txt', 'utf8', (err, data) => {
     // Разбиваем строку на массив цветов и преобразуем в верхний регистр
     const inputColors = data.split(',').map(color => color.trim().replace(/^#/, '').toUpperCase());
 
-    // Преобразуем каждый цвет и находим ближайший цвет из палитры
-    const results = inputColors.map(color => findNearestColor(`#${color}`)).map(line => line + ";");
-
-    // Собираем результаты в одну строку с разделителями
+    // Находим ближайший цвет для каждого входного цвета и добавляем в указанный формат
+    const results = inputColors.map(color => findNearestColor(`#${color}`)).filter(Boolean); // Фильтруем пустые результаты
     const output = results.join('\n');
 
-    // Запись результатов в файл
+    // Записываем результаты в файл вывода
     fs.writeFile('colorDoneVar.txt', output, 'utf8', err => {
         if (err) {
             console.error('Ошибка при записи файла:', err);
